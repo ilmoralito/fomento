57ff62d5c1054d0f29bf7f49ee83c8a4cfc57f67
mario martinez <amakenadog@gmail.com>
2013-09-17 13:18:06 -0600

    set default configuration

---
+++ src/templates/artifacts/Controller.groovy
@@ -0,0 +1,10 @@
+@artifact.package@class @artifact.name@ {
+
+	static defaultAction = "actionName"
+	static allowedMethods = []
+
+    def actionName() {
+
+    }
+
+}
---
+++ src/templates/artifacts/DomainClass.groovy
@@ -0,0 +1,27 @@
+@artifact.package@class @artifact.name@ {
+
+
+
+	Date dateCreated
+	Date lastUpdated
+
+    static constraints = {
+
+    }
+
+    static namedQueries = {
+
+    }
+
+    static hasMany = []
+    static belongsTo = []
+
+    static mapping = {
+
+    }
+
+    String toString() {
+
+    }
+
+}
