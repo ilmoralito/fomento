49389aacd862c729a978d3dc941141302c70b2d7
mario martinez <amakenadog@gmail.com>
2013-09-19 14:38:13 -0600

    created test users

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,27 @@
+import org.fomento.*
+import grails.util.GrailsUtil
+
 class BootStrap {
 
     def init = { servletContext ->
+    	switch(GrailsUtil.environment) {
+    		case "development":
+                //roles
+                def adminRole = new Role(authority:"ROLE_ADMIN").save()
+                def userRole = new Role(authority:"ROLE_USER").save()
+
+                //users
+                def testAdminUser = new User(username:"me", enabled:true, password:"password").save()
+                def testUserUser = new User(username:"ich", enabled:true, password:"password").save()
+
+                UserRole.create testAdminUser, adminRole, true
+                UserRole.create testUserUser, userRole, true
+
+                //test
+                assert User.count() == 2
+                assert Role.count() == 2
+    		break
+    	}
     }
     def destroy = {
     }
