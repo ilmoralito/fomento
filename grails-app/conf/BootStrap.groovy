621d60bb8084abac986a7c8dfda6d185c27a68fb
mario martinez <amakenadog@gmail.com>
2013-09-19 15:04:05 -0600

    created prod user and role

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,12 +4,12 @@ import grails.util.GrailsUtil
 class BootStrap {
 
     def init = { servletContext ->
+        //roles
+        def adminRole = (Role.findByAuthority("ROLE_ADMIN")) ?: new Role(authority:"ROLE_ADMIN").save()
+        def userRole = (Role.findByAuthority("ROLE_USER")) ?: new Role(authority:"ROLE_USER").save()
+
     	switch(GrailsUtil.environment) {
     		case "development":
-                //roles
-                def adminRole = new Role(authority:"ROLE_ADMIN").save()
-                def userRole = new Role(authority:"ROLE_USER").save()
-
                 //users
                 def testAdminUser = new User(username:"me", enabled:true, password:"password").save()
                 def testUserUser = new User(username:"ich", enabled:true, password:"password").save()
@@ -20,6 +20,11 @@ class BootStrap {
                 //test
                 assert User.count() == 2
                 assert Role.count() == 2
+    		break
+    		case "production":
+    			def prodAdminUser = (User.findByUsername("me")) ?: new User(username:"me", enabled:true, password:"123").save()
+
+    			UserRole.create prodAdminUser, adminRole, true
     		break
     	}
     }
