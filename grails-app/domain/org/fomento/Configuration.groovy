94abfbb46633ed6ee2112d78ee6472e25985d02f
mario martinez <amakenadog@gmail.com>
2013-09-26 08:42:29 -0600

    created configuration domain class and controller

---
+++ grails-app/domain/org/fomento/Configuration.groovy
@@ -0,0 +1,29 @@
+package org.fomento
+
+class Configuration {
+
+
+
+	Date dateCreated
+	Date lastUpdated
+
+    static constraints = {
+
+    }
+
+    static namedQueries = {
+
+    }
+
+    static hasMany = []
+    static belongsTo = []
+
+    static mapping = {
+
+    }
+
+    String toString() {
+
+    }
+
+}
