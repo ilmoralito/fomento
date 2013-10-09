da3c4d3be66a9cb56e399e468a1b588b2a53c0eb
mario martinez <amakenadog@gmail.com>
2013-10-09 09:09:43 -0600

    created deduction domain class and controller

---
+++ grails-app/domain/org/fomento/Deduction.groovy
@@ -0,0 +1,29 @@
+package org.fomento
+
+class Deduction {
+
+
+
+	Date dateCreated
+	Date lastUpdated
+
+    static constraints = {
+
+    }
+
+    static namedQueries = {
+
+    }
+
+    static hasMany = []
+    static belongsTo = []
+
+    static mapping = {
+
+    }
+
+    String toString() {
+
+    }
+
+}
