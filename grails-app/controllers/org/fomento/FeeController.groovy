79fbc395779c30dd37f42af3245f38965bbfd114
mario martinez <amakenadog@gmail.com>
2013-11-04 10:36:02 -0600

    removed fee from partner info

--- grails-app/controllers/org/fomento/FeeController.groovy
+++ grails-app/controllers/org/fomento/FeeController.groovy
@@ -1,37 +1,15 @@
 package org.fomento
 
 import grails.plugins.springsecurity.Secured
-import static java.util.Calendar.*
 
 @Secured(['ROLE_ADMIN', 'ROLE_USER', 'IS_AUTHENTICATED_FULLY'])
 class FeeController {
 
     def feeService
 
-	static defaultAction = "list"
+	static defaultAction = "someAction"
 	static allowedMethods = [
-		"list":"GET"
+		"someAction":"GET"
 	]
-
-    def list(Integer id, String year) {
-    	def partner = Partner.get(params?.id)
-        def now = new Date()
-        def currentYear = now[YEAR]
-        def period = (params.int("year")) ?: currentYear
-
-    	if (!partner) {
-    		response.sendError 404
-    	}
-
-        def fees = Fee.byPeriod(period).findAllByPartner(partner,[sort:"paymentDate", order:"desc"])
-
-    	[
-            fees:fees,
-            period:period,
-            partner:partner,
-            total:feeService.calcTotal(fees, partner),
-            totalFactoryFee:feeService.calcFactoryTotalFeesByPartner(fees)
-        ]
-    }
 
 }
