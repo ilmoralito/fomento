568279e50eb5a769015b6931b04a81b092cb29cc
mario martinez <amakenadog@gmail.com>
2013-11-17 11:46:33 -0600

    fixed, save pds and pde in scale 4

--- grails-app/domain/org/fomento/Dividend.groovy
+++ grails-app/domain/org/fomento/Dividend.groovy
@@ -27,8 +27,8 @@ class Dividend {
         tas blank:false, min:0.0
         tae blank:false, min:0.0
         tap blank:false, min:0.0
-        pds blank:false, min:0.0
-        pde blank:false, min:0.0
+        pds blank:false, min:0.00, scale:4
+        pde blank:false, min:0.00, scale:4
         period blank:false, min:2013
         up blank:false, min:1.0
         capitalization nullable:true
