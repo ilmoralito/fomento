8584ecc8d71606e2cf4e5be457de14fdd7e35096
Mario Martinez <amakenadog@gmail.com>
2014-01-19 10:47:39 -0600

    Refactored capitalize

--- web-app/js/capitalize.js
+++ web-app/js/capitalize.js
@@ -8,15 +8,25 @@ percentage.on("input change",function(){
 	var per = 100 - this.value
 	range.html(per);
 
-	capitalized.html(capital * (per / 100))
+	capitalized.html(get_total_to_capitalize(capital, per).toFixed(3))
 
-	nocapitalized.html(capital - (capital * (per / 100)))
+	nocapitalized.html(get_total_to_no_capitalize(capital, per).toFixed(3))
 });
 
 //on load
 var per = 100 - percentage.val()
 range.html(per);
 
-capitalized.html(capital * (per / 100))
+capitalized.html(get_total_to_capitalize(capital, per).toFixed(3))
 
-nocapitalized.html(capital - (capital * (per / 100)))
+nocapitalized.html(get_total_to_no_capitalize(capital, per).toFixed(3))
+
+function get_total_to_capitalize(capital, percentage){
+	return capital * (percentage / 100)
+}
+
+function get_total_to_no_capitalize(capital, percentage){
+	var to_capitalize = get_total_to_capitalize(capital, percentage)
+
+	return capital - to_capitalize
+}
